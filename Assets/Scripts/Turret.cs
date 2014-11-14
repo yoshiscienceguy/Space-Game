9190f25c59d3593ab65d8ebde0e81aaf7d371316
JustinRChou <justinrchou@gmail.com>
2014-11-13 23:23:31 -0800

    turret + deletes

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -7,13 +7,16 @@ public class Turret : MonoBehaviour {
 	public float minAngle = 0f;
 	public float maxAngle = 180f;
 	private float dir = 1f;
+	public bool aimPlayer = false;
 	// Use this for initialization
 	void Start () {
-
+		Debug.Log (this.gameObject.transform.rotation.z);
+		transform.rotation =  Quaternion.Euler(0, 0, minAngle);;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Debug.Log (transform.forward);
 		if (transform.rotation.z >= maxAngle) {
 						dir *= -1f;
 				} else if (transform.rotation.z <= minAngle) {
